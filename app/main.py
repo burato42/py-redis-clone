d4db66da97553bda424d253744cc7cba8a921fcd
burato42 <tmahmutov@gmail.com>
2025-09-29 17:59:36 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -8,7 +8,10 @@ def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     connection, _ = server_socket.accept() # wait for client
 
-    connection.sendall(b"+PONG\r\n")
+    while True:
+        data = connection.recv(1024).decode()
+        if "PING" in data:
+            connection.sendall(b"+PONG\r\n")
 
 
 if __name__ == "__main__":
